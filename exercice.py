0eb4627cdaf40ba111f24a3c9da56e8380ba07f3
mathildebrosseau <70984337+mathildebrosseau@users.noreply.github.com>
2020-10-05 19:54:20 -0400

    fin cours 5 oct

--- exercice.py
+++ exercice.py
@@ -7,30 +7,44 @@ from matplotlib.colors import cnames
 def list_to_dict(some_list: list) -> dict:
     # TODO: Transformer la liste en dictionnaire, les éléments de la liste deviennent les clés et leur index deviennent les valeurs
     dictionary = dict()
-    for element in some_list:
-        dictionary[some_list.index(element)] = element
+    for index, element in enumerate(some_list):
+        dictionary[element] = index
     return dictionary
 
 
 def color_name_to_hex(colors: list) -> list:
     # TODO: Trouver la valeur hex de chaque couleur dans la liste et créer une liste de tupple où le premier élément est le nom de la couleur et le deuxième est la valeur hex
     color_list = list()
-    for index, color in enumerate(colors):
-        color_hexindex = (hex(index), color)
-        color_list.append(color_hexindex)
+    for c in colors:
+        if c in cnames:
+            color_list.append((c, cnames[c]))
+        else:
+            print("La couleur {c} n'existe pas dans notre dictionnaire.")
     return color_list
 
 
 def create_list() -> list:
     # TODO: Créer une liste des 10 000 premiers entiers positif, sauf pour les entiers de 15 à 350
-
-    return []
+    """
+       # La mauvaise façon
+          entierspositifs = list()
+       for i in range(10000):
+           if i < 15 and i < 350:
+               entierspositifs.append(i)
+           return entierspositifs
+       """
+    # La bonne façon
+    return [number for number in range(10000) if number < 15 or number > 350]
 
 
 def compute_mse(model_dict: dict) -> dict:
     # TODO: Calculer l'erreur quadratique moyen pour chaque modèle. Retourner un dictionnaire contenant les MSE.
-
-    return {}
+    for key, value in model_dict.items():
+        err_quad = 0
+        for element in value:
+            err_quad += (element[0] - element[1]) ** 2
+        model_dict[key] = err_quad / len(value)
+    return model_dict
 
 
 def main() -> None:
